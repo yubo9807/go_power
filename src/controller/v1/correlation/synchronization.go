cc6adcc0637303babdb17f7f5f9435a640e56420
yangyb <yangyb@boyachain.cn>
2023-01-04 21:15:43 +0800

    fix: 搜索条件下分配权限bug

--- src/controller/v1/correlation/synchronization.go
+++ src/controller/v1/correlation/synchronization.go
@@ -10,10 +10,10 @@ import (
 // 同步权限
 func Synchronization(ctx *gin.Context) {
 	type Params struct {
-		RoleId      string   `form:"roleId" binding:"required"`
-		TableIdList []string `form:"tableIdList" binding:"required"`
-		TableType   string   `form:"tableType" binding:"required"`
-		MenuId      string   `form:"menuId"`
+		RoleId        string   `form:"roleId" binding:"required"`
+		ContactIdList []string `form:"contactIdList" binding:"required"`
+		DeleteIdList  []string `form:"deleteIdList" binding:"required"`
+		TableType     string   `form:"tableType" binding:"required"`
 	}
 	var params Params
 	if err := ctx.ShouldBind(&params); err != nil {
@@ -21,18 +21,13 @@ func Synchronization(ctx *gin.Context) {
 		return
 	}
 
-	newTableIdList := make([]string, 0)
-	var rows []spider.Correlation
-	if params.TableType == "menu" {
-		rows = spider.CorrelationTableTypeQuery(params.RoleId, params.TableType)
-	} else {
-		rows = spider.CorrelationTableTypeQuery2(params.RoleId, params.TableType, params.MenuId)
-	}
+	newContactIdList := make([]string, 0)
+	rows := spider.CorrelationTableTypeQuery(params.RoleId, params.TableType)
 
-	for i := 0; i < len(params.TableIdList); i++ {
+	for i := 0; i < len(params.ContactIdList); i++ {
 		exist := false
 		for j := 0; j < len(rows); j++ {
-			exist = params.TableIdList[i] == rows[j].RoleId && params.TableIdList[i] == rows[j].TableId
+			exist = params.ContactIdList[i] == rows[j].RoleId && params.ContactIdList[i] == rows[j].TableId
 			if exist {
 				break
 			}
@@ -40,25 +35,10 @@ func Synchronization(ctx *gin.Context) {
 		if exist { // 已经存在
 			continue
 		}
-		newTableIdList = append(newTableIdList, params.TableIdList[i])
+		newContactIdList = append(newContactIdList, params.ContactIdList[i])
 	}
 
-	newDleTableIdList := make([]string, 0)
-	for i := 0; i < len(rows); i++ {
-		exist := false
-		for j := 0; j < len(newTableIdList); j++ {
-			exist = rows[i].TableId == newTableIdList[j]
-			if exist {
-				break
-			}
-		}
-		if exist {
-			continue
-		}
-		newDleTableIdList = append(newDleTableIdList, rows[i].TableId)
-	}
-
-	spider.CorrelationBatchAdditional(params.TableType, params.RoleId, newTableIdList, newDleTableIdList)
+	spider.CorrelationBatchAdditional(params.TableType, params.RoleId, newContactIdList, params.DeleteIdList)
 	service.Success()
 
 }
