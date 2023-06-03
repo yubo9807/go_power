e1128991c8f3e33e2a0c292bb2e15f0886ca1231
yangyb <yangyb@boyachain.cn>
2023-06-03 17:15:45 +0800

    delete: core origin

--- src/middleware/core.go
+++ src/middleware/core.go
@@ -1,18 +1,10 @@
 package middleware
 
 import (
-	"server/configs"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Core(ctx *gin.Context) {
-	origin := "http://hpyyb.cn"
-	if configs.Env.DEVELOPMENT {
-		origin = "*"
-	}
-
-	ctx.Header("Access-Control-Allow-Origin", origin)
 	ctx.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
 
