995c0519fd361140cccf15cf3350ea68ac7f2a35
yangyb <yangyb@boyachain.cn>
2023-06-05 21:57:31 +0800

    feat: 并发控制

--- src/controller/v1/menu/additional.go
+++ src/controller/v1/menu/additional.go
@@ -3,9 +3,13 @@ package menu
 import (
 	"server/src/service"
 	"server/src/spider"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
+
+var mu sync.Mutex
+var wg sync.WaitGroup
 
 // 添加菜单
 func Additional(ctx *gin.Context) {
@@ -21,14 +25,23 @@ func Additional(ctx *gin.Context) {
 		return
 	}
 
-	// 已存在的菜单不允许添加
-	rows := spider.Menu.Query(params.Name, "")
-	if len(rows) > 0 {
-		service.State.ErrorCustom("菜单'" + params.Name + "'已存在")
-		return
-	}
+	wg.Add(1)
+	go func() {
+		mu.Lock()
 
-	// 添加菜单
-	spider.Menu.Additional(params.Name, params.Title, params.Hidden, params.Parent)
+		// 已存在的菜单不允许添加
+		rows := spider.Menu.Query(params.Name, "")
+		if len(rows) > 0 {
+			service.State.ErrorCustom("菜单'" + params.Name + "'已存在")
+			return
+		}
+		// 添加菜单
+		spider.Menu.Additional(params.Name, params.Title, params.Hidden, params.Parent)
+
+		wg.Done()
+		mu.Unlock()
+	}()
+	wg.Wait()
+
 	service.State.Success()
 }
