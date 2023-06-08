a2e1bee72fd1090e371403b980082c6a1952d303
yangyb <yangyb@boyachain.cn>
2023-06-08 08:44:56 +0800

    fix: core

--- src/middleware/core.go
+++ src/middleware/core.go
@@ -1,14 +1,18 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Core(ctx *gin.Context) {
-	ctx.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+	// ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, UPDATE")
 	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("Access-Control-Allow-Credentials", "true")
 
 	if ctx.Request.Method == "OPTIONS" {
-		ctx.String(200, "ok")
+		ctx.AbortWithStatus(http.StatusNoContent)
 	}
 }
