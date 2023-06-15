05877f01da29c2247b555187f897a86cc37db024
yangyb <yangyb@boyachain.cn>
2023-06-15 21:51:35 +0800

    feat: 工具函数-获取结构体 db 值

--- src/utils/string.go
+++ src/utils/string.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
+	"reflect"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -22,4 +25,31 @@ func StringRandomSort(str string) string {
 		arr[i], arr[j] = arr[j], arr[i]
 	})
 	return string(arr)
+}
+
+// 获取结构体中定义的 db 值
+func GetStructDBKeys(stru interface{}) []string {
+	t := reflect.TypeOf(stru) // 获取结构体类型
+
+	if t.Kind() != reflect.Struct {
+		fmt.Println("Input is not a struct")
+		return nil
+	}
+
+	numFields := t.NumField() // 结构体字段数量
+	dbTags := make([]string, 0, numFields)
+
+	// 遍历结构体的字段
+	for i := 0; i < numFields; i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("db")
+		if tag == "" {
+			name := strings.ToLower(field.Name)
+			dbTags = append(dbTags, name)
+		} else {
+			dbTags = append(dbTags, tag)
+		}
+	}
+
+	return dbTags
 }
