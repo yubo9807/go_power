c3a65fc740ae1097d477be91d9acb9a5575d3b06
yangyb <yangyb@boyachain.cn>
2023-06-15 21:35:22 +0800

    fix: 记录 sql 阻塞问题

--- src/middleware/bodyDispose.go
+++ src/middleware/bodyDispose.go
@@ -10,8 +10,10 @@ func BodyDispose(ctx *gin.Context) {
 	service.State.InitState(ctx)
 
 	go func() {
-		sql := <-service.ChSql
-		service.State.RecordSql(ctx, sql[0], sql[1])
+		for i := 0; i <= 5; i++ { // 多开启几个接收通道，防止阻塞
+			sql := <-service.ChSql
+			service.State.RecordSql(ctx, sql[0], sql[1])
+		}
 	}()
 
 	ctx.Next()
